6ca5282902c12e4e7cbcaf86addbe5e955f56319
tjalkane <1162417+Koura@users.noreply.github.com>
2019-06-16 22:16:16 +0300

    Feat: implement server  with actix-web

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@
 
 pub mod blockchain;
 
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world!"
+}
+
 fn main() {
     let mut blockchain = blockchain::Blockchain::new();
     let block = blockchain.new_block(100, Some("2"));
     println!("{}", blockchain::Blockchain::hash(&block));
     println!("{}", blockchain::Blockchain::proof_of_work(1));
+
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run();
 }
