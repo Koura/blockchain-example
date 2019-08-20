3c5afc62c5f89ae1092819b574a7c7e4ce670504
tjalkane <1162417+Koura@users.noreply.github.com>
2019-08-20 23:29:24 +0300

    Feat: add implementation for transactions/new endpoint

--- src/api.rs
+++ src/api.rs
@@ -5,18 +5,36 @@ use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
 #[derive(Serialize, Deserialize)]
-pub struct Info {
+pub struct TransactionResponse {
     message: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct TransactionRequest {
+    sender: String,
+    recipient: String,
+    amount: i64,
+}
+
 #[derive(Serialize)]
-pub struct Chain<'a> {
-    chain: Vec<Block<'a>>,
+pub struct Chain {
+    chain: Vec<Block>,
     length: usize,
 }
 
-pub fn new_transaction(req: web::Json<Info>) -> HttpResponse {
-    HttpResponse::Ok().json(req.0)
+pub fn new_transaction(
+    state: web::Data<Mutex<Blockchain>>,
+    req: web::Json<TransactionRequest>,
+) -> HttpResponse {
+    let sender = req.sender.to_owned();
+    let recipient = req.recipient.to_owned();
+    let index = state
+        .lock()
+        .unwrap()
+        .new_transaction(&sender, &recipient, req.amount);
+    HttpResponse::Created().json(TransactionResponse {
+        message: format! {"Transaction will be added to Block {}", index},
+    })
 }
 
 pub fn mine(_req: HttpRequest) -> HttpResponse {
--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,29 +3,29 @@ use crypto_hash::{hex_digest, Algorithm};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Hash, Serialize, Deserialize)]
-struct Transaction<'a> {
-    sender: &'a str,
-    recipient: &'a str,
+struct Transaction {
+    sender: String,
+    recipient: String,
     amount: i64,
 }
 
 #[derive(Clone, Hash, Serialize, Deserialize)]
-pub struct Block<'a> {
+pub struct Block {
     index: u64,
     timestamp: DateTime<Utc>,
-    transactions: Vec<Transaction<'a>>,
+    transactions: Vec<Transaction>,
     proof: u64,
-    previous_hash: &'a str,
+    previous_hash: String,
 }
 
 #[derive(Default)]
-pub struct Blockchain<'a> {
-    pub chain: Vec<Block<'a>>,
-    current_transaction: Vec<Transaction<'a>>,
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+    current_transaction: Vec<Transaction>,
 }
 
-impl<'a> Blockchain<'a> {
-    pub fn new() -> Blockchain<'a> {
+impl Blockchain {
+    pub fn new() -> Blockchain {
         let mut blockchain = Blockchain {
             chain: vec![],
             current_transaction: vec![],
@@ -38,13 +38,13 @@ impl<'a> Blockchain<'a> {
     /// :param proof: The proof given by the Proof of Work algorithm
     /// :param previous_hash: (Optional) hash of previous Block
     /// :return: New Block
-    pub fn new_block(&mut self, proof: u64, previous_hash: Option<&'a str>) -> Block {
+    pub fn new_block(&mut self, proof: u64, previous_hash: Option<&str>) -> Block {
         let block = Block {
             index: (self.chain.len() + 1) as u64,
             timestamp: Utc::now(),
             transactions: self.current_transaction.drain(0..).collect(),
             proof,
-            previous_hash: previous_hash.unwrap_or("0"),
+            previous_hash: previous_hash.unwrap_or("0").to_string(),
         };
 
         self.chain.push(block.clone());
@@ -56,10 +56,10 @@ impl<'a> Blockchain<'a> {
     /// :param recipient: Address of the recipient
     /// :param amount: Amount
     /// :return: The index of the Block that will hold this transaction
-    pub fn new_transaction(&mut self, sender: &'a str, recipient: &'a str, amount: i64) -> u64 {
+    pub fn new_transaction(&mut self, sender: &str, recipient: &str, amount: i64) -> u64 {
         self.current_transaction.push(Transaction {
-            sender: &sender,
-            recipient: &recipient,
+            sender: sender.to_string(),
+            recipient: recipient.to_string(),
             amount,
         });
         self.last_block() + 1
