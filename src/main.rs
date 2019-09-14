501e2ef37a289fcd00e3204851c39ee9d0a0294e
tjalkane <1162417+Koura@users.noreply.github.com>
2019-09-14 21:55:32 +0300

    Feat: add possibility to pass port via commandline argumments

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@ pub mod api;
 pub mod blockchain;
 
 use actix_web::{web, App, HttpServer};
+use std::env;
 use std::sync::Mutex;
 use uuid::Uuid;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let port = match args.as_slice() {
+        [_, key, value] => {
+            if (key == "--p") {
+                value
+            } else {
+                panic!("Illegal arguments passed to the program.");
+            }
+        }
+        _ => "5000",
+    };
     // TODO: make chain shared across threads
     let sharedchain = web::Data::new(Mutex::new(blockchain::Blockchain::new()));
     let node_identifier = web::Data::new(Uuid::new_v4().to_simple().to_string());
@@ -23,7 +35,7 @@ fn main() {
             .service(web::resource("/nodes/register").route(web::post().to(api::register_node)))
             .service(web::resource("/nodes/resolve").route(web::get().to(api::resolve_nodes)))
     })
-    .bind("127.0.0.1:5000")
+    .bind(format!("127.0.0.1:{}", port))
     .unwrap()
     .run();
 }
